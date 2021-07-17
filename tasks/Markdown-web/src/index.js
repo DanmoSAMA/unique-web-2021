a5eea3edea360f6ac823e07817542a07ba865bef
DanmoSAMA <772523546@qq.com>
2021-07-17 22:13:22 +0800

    7.17

--- tasks/Markdown-web/src/index.js
+++ tasks/Markdown-web/src/index.js
@@ -1,84 +1,197 @@
+// 使用ajax读取配置文件
+$.ajax({
+  type: "get",
+  url: "../config.json",
+  dataType: "json",
+  success: function (data) {
+    if (data.theme === "dark") {
+      document.body.className = "dark";
+    }
+    if (data['font-size'] === "small") {
+      document.getElementById("editor-mid-layer").className = "small";
+      document.getElementById("content-wrapper").className = "small";
+    }
+    else if (data['font-size'] === "large") {
+      document.getElementById("editor-mid-layer").className = "large";
+      document.getElementById("content-wrapper").className = "large";
+    }
+  },
+  error: function (err) {
+    console.error(err);
+  }
+});
+
 const ta = document.getElementById("TA");
+const ip = document.getElementById("IP");
+const downloadBtn = document.getElementById("download");
 const contentWrapper = document.getElementById("content-wrapper");
+
+const titleReg = /^\s{0,3}#{1,6}\s/;
+const sharpReg = /^\s{0,3}#{1,6}/;
+const enterReg = /\n$/;
+const italicReg = /\*\S.*\*/;
+const boldReg = /\*{2}\S.*\*{2}/;
+const hyperReg = /\[.+\]\(.+\)/;
+
+const liReg = /^[\*\-]\s/;
+const liChildrenReg = /^\s{4}[\*\-]\s/;
 const numReg = /^[0-9]{1,}\. /;
-const italicReg = /^\*\S.*\*$/;
-const boldReg = /^\*{2}\S.*\*{2}$/;
+const numChildenReg = /^\s{4}[0-9]{1,}\. /;
 
 ta.addEventListener('input', setValue);
 ta.addEventListener('keydown', prevTab);
+ip.addEventListener('input', saveTitle);
+
+// 读取localStorage
+let htmlStr = localStorage.getItem('htmlStr');
+if (htmlStr !== null) {
+  ta.value = htmlStr;
+  parseEl();
+  setValue();
+}
+
+// 读取title
+let title = localStorage.getItem('title');
+ip.value = title;
+
+//下载
+downloadBtn.addEventListener('click', () => {
+  download(htmlStr, `${title}.md`);    
+});
 
 // 解析html字符串
-function parse() {
+function parseEl() {
   let htmlStr = ta.value;
   let htmlArr = htmlStr.split('\n');
   let firstMeetLi = true;
   let isOrder = new Array;
 
+  localStorage.setItem('htmlStr', htmlStr);
+
   for (let i = 0; i < htmlArr.length; i++) {
-    if (htmlArr[i].startsWith('# ')) {
-      htmlArr[i] = `<h1>${htmlArr[i].substring(1)}</h1>`;
+    // 标题
+    if (titleReg.test(htmlArr[i])) {
+      const titleReg2 = /^\s{0,3}#{2}\s/; const titleReg3 = /^\s{0,3}#{3}\s/;
+      const titleReg4 = /^\s{0,3}#{4}\s/; const titleReg5 = /^\s{0,3}#{5}\s/;
+      const titleReg6 = /^\s{0,3}#{6}\s/;
+
+      if (titleReg2.test(htmlArr[i])) {
+        htmlArr[i] = `<h2>${htmlArr[i]}</h2>`;
+        htmlArr[i] = htmlArr[i].replace(/##/, "");
+      }
+      else if (titleReg3.test(htmlArr[i])) {
+        htmlArr[i] = `<h3>${htmlArr[i]}</h3>`;
+        htmlArr[i] = htmlArr[i].replace(/###/, "");
+      }
+      else if (titleReg4.test(htmlArr[i])) {
+        htmlArr[i] = `<h4>${htmlArr[i]}</h4>`;
+        htmlArr[i] = htmlArr[i].replace(/####/, "");
+      }
+      else if (titleReg5.test(htmlArr[i])) {
+        htmlArr[i] = `<h5>${htmlArr[i]}</h5>`;
+        htmlArr[i] = htmlArr[i].replace(/#####/, "");
+      }
+      else if (titleReg6.test(htmlArr[i])) {
+        htmlArr[i] = `<h6>${htmlArr[i]}</h6>`;
+        htmlArr[i] = htmlArr[i].replace(/######/, "");
+      }
+      else {
+        htmlArr[i] = `<h1>${htmlArr[i]}</h1>`;
+        htmlArr[i] = htmlArr[i].replace(/#/, "");
+      }
     }
-    else if (htmlArr[i].startsWith('## ')) {
-      htmlArr[i] = `<h2>${htmlArr[i].substring(2)}</h2>`;
-    }
-    else if (htmlArr[i].startsWith('### ')) {
-      htmlArr[i] = `<h3>${htmlArr[i].substring(3)}</h3>`;
-    }
-    else if (htmlArr[i].startsWith('#### ')) {
-      htmlArr[i] = `<h4>${htmlArr[i].substring(4)}</h4>`;
-    }
-    else if (htmlArr[i].startsWith('##### ')) {
-      htmlArr[i] = `<h5>${htmlArr[i].substring(5)}</h5>`;
-    }
-    else if (htmlArr[i].startsWith('###### ')) {
-      htmlArr[i] = `<h6>${htmlArr[i].substring(6)}</h6>`;
-    }
-    else if (htmlArr[i].startsWith('* ') || htmlArr[i].startsWith('- ')) {
-      htmlArr[i] = `<li>${htmlArr[i].substring(1)}</li>`;
+    // 无序列表
+    else if (liReg.test(htmlArr[i])) {
+      htmlArr[i] = htmlArr[i].replace(liReg, "");
+      htmlArr[i] = `<li>${htmlArr[i]}</li>`;
       isOrder[i] = false;
     }
-    else if (htmlArr[i].startsWith('    * ') || htmlArr[i].startsWith('    - ')) {
-      htmlArr[i] = `<li class="children">${htmlArr[i].substring(5)}</li>`;
+    else if (liChildrenReg.test(htmlArr[i])) {
+      htmlArr[i] = htmlArr[i].replace(liChildrenReg, "");
+      htmlArr[i] = `<li class="children">${htmlArr[i]}</li>`;
+      isOrder[i] = false;
     }
-    else if (htmlArr[i].startsWith('        * ') || htmlArr[i].startsWith('       - ')) {
-      htmlArr[i] = `<li class="children">${htmlArr[i].substring(9)}</li>`;
-    }
+    // 有序列表
     else if (numReg.test(htmlArr[i])) {
+      htmlArr[i] = htmlArr[i].replace(numReg, "");
+      htmlArr[i] = `<li>${htmlArr[i]}</li>`;
       isOrder[i] = true;
-      htmlArr[i] = `<li>${htmlArr[i].substring(2)}</li>`;
     }
+    else if (numChildenReg.test(htmlArr[i])) {
+      htmlArr[i] = htmlArr[i].replace(numChildenReg, "");
+      htmlArr[i] = `<li class="children">${htmlArr[i]}</li>`;
+      isOrder[i] = true;
+    }
+    // 分割线
     else if (htmlArr[i].startsWith('***')) {
       htmlArr[i] = `<hr>`;
     }
-    else if (italicReg.test(htmlArr[i]) && !boldReg.test(htmlArr[i])) {
-      htmlArr[i] = `<i>${htmlArr[i].substring(1, htmlArr[i].length - 1)}</i>`
+    // 斜体、粗体
+    else if (italicReg.test(htmlArr[i]) || boldReg.test(htmlArr[i])) {
+      const starReg1 = /\*/;
+      const starReg2 = /\*{2}/;
+      while (starReg2.test(htmlArr[i])) {
+        htmlArr[i] = htmlArr[i].replace(/\*{2}/, "<b>");
+        htmlArr[i] = htmlArr[i].replace(/\*{2}/, "</b>");
+      }
+      while (starReg1.test(htmlArr[i])) {
+        htmlArr[i] = htmlArr[i].replace(/\*{1}/, "<i>");
+        htmlArr[i] = htmlArr[i].replace(/\*{1}/, "</i>");
+      }
+      htmlArr[i] += '<br>';
     }
-    else if (boldReg.test(htmlArr[i])) {
-      htmlArr[i] = `<b>${htmlArr[i].substring(2, htmlArr[i].length - 2)}</b>`
+    // 超链接
+    else if (hyperReg.test(htmlArr[i])) {
+      const hyperReg1 = /\[.+\]/;
+      const hyperReg2 = /\(.+\)/;
+      let infoArr = htmlArr[i].match(hyperReg1);
+      let urlArr = htmlArr[i].match(hyperReg2);
+
+      let infoStr = infoArr[0].replace(/\[/, "").replace(/\]/, "");
+      let urlStr = urlArr[0].replace(/\(/, "").replace(/\)/, "");
+
+      htmlArr[i] = htmlArr[i].replace(/\[.+\]\(.+\)/, `<a href='${urlStr}'>${infoStr}</a>`);
     }
     else if (htmlArr[i] === "") {
       htmlArr[i] = '<br/>';
     }
     else {
+      // 没有遇到\时，#的解析方式：
+      if (sharpReg.test(htmlArr[i])) {
+        const sharpReg1 = /^\s{0,3}#{1,6}[^#]+/;
+        const sharpReg2 = /^\s{0,3}#{7,}/;
+        if (!sharpReg1.test(htmlArr[i]) && !sharpReg2.test(htmlArr[i])) {
+          htmlArr[i] = htmlArr[i].replace(/#{1,6}/, "");
+        }
+      }
+      // 转义
+      else if (htmlArr[i].startsWith('\\')) {
+        htmlArr[i] = htmlArr[i].replace(/\\/, "");
+      }
       htmlArr[i] = `<p>${htmlArr[i]}</p>`;
     }
   }
 
-  // 将li标签放入ul或ol(bug之后改)
+  // 将li标签放入ul或ol
   for (let i = 0; i < htmlArr.length; i++) {
     if (htmlArr[i].startsWith('<li')) {
+      // 父li
       if (htmlArr[i].startsWith('<li>')) {
-        if (isOrder[i] === false) {
+        // 无序列表
+        if (!isOrder[i]) {
+          // 第一个父li
           if (firstMeetLi) {
-            htmlArr[i] = `<ul>${htmlArr[i]}`;
+            htmlArr[i] = `<ul class="fatherUl">${htmlArr[i]}`;
           }
-          if (htmlArr[i + 1] && !htmlArr[i + 1].startsWith('<li')) {
+          // 最后一个父li
+          if (!htmlArr[i + 1] || !htmlArr[i + 1].startsWith('<li')) {
             htmlArr[i] = `${htmlArr[i]}</ul>`;
           }
         }
+        // 有序列表
         else {
           if (firstMeetLi) {
-            htmlArr[i] = `<ol>${htmlArr[i]}`;
+            htmlArr[i] = `<ol class="fatherOl">${htmlArr[i]}`;
           }
           if (htmlArr[i + 1] && !htmlArr[i + 1].startsWith('<li')) {
             htmlArr[i] = `${htmlArr[i]}</ol>`;
@@ -86,16 +199,42 @@ function parse() {
         }
         firstMeetLi = false;
       }
-      // 一级子元素
+      // 子li
       else if (htmlArr[i].startsWith('<li class="children">')) {
-        if (htmlArr[i - 1].startsWith('<li>')) {
-          htmlArr[i] = `<ul>${htmlArr[i]}`;
+        if (!isOrder[i]) {
+          // 第一个子li
+          if (htmlArr[i - 1].startsWith('<ul class="fatherUl"><li>')) {
+            htmlArr[i - 1] = htmlArr[i - 1].replace(`<ul class="fatherUl"><li>`, `<ul class="fatherUl"><li class="father">`);
+            htmlArr[i] = `<ul class="childrenUl">${htmlArr[i]}`;
+          }
+          if (htmlArr[i - 1].startsWith('<li>')) {
+            htmlArr[i - 1] = htmlArr[i - 1].replace(`<li>`, `<li class="father">`);
+            htmlArr[i] = `<ul class="childrenUl">${htmlArr[i]}`;
+          }
+          // 最后一个子li，下边还有父li
+          if (htmlArr[i + 1] && htmlArr[i + 1].startsWith('<li>')) {
+            htmlArr[i] = `${htmlArr[i]}</ul>`;
+          }
+          // 最后一个子li，也是最后一个li
+          else if (!htmlArr[i + 1] || !htmlArr[i + 1].startsWith('<li')) {
+            htmlArr[i] = `${htmlArr[i]}</ul></ul>`;
+          }
         }
-        if (htmlArr[i + 1] && htmlArr[i + 1].startsWith('<li>')) {
-          htmlArr[i] = `${htmlArr[i]}</ul>`;
-        }
-        else if (i === htmlArr.length - 1) {
-          htmlArr[i] = `${htmlArr[i]}</ul>`;
+        else {
+          if (htmlArr[i - 1].startsWith('<ol class="fatherOl"><li>')) {
+            htmlArr[i - 1] = htmlArr[i - 1].replace(`<ol class="fatherOl"><li>`, `<ol class="fatherOl"><li class="father">`);
+            htmlArr[i] = `<ol class="childrenOl">${htmlArr[i]}`;
+          }
+          if (htmlArr[i - 1].startsWith('<li>')) {
+            htmlArr[i - 1] = htmlArr[i - 1].replace(`<li>`, `<li class="father">`);
+            htmlArr[i] = `<ol class="childrenOl">${htmlArr[i]}`;
+          }
+          if (htmlArr[i + 1] && htmlArr[i + 1].startsWith('<li>')) {
+            htmlArr[i] = `${htmlArr[i]}</ol>`;
+          }
+          else if (!htmlArr[i + 1] || !htmlArr[i + 1].startsWith('<li')) {
+            htmlArr[i] = `${htmlArr[i]}</ol></ol>`;
+          }
         }
       }
     }
@@ -103,22 +242,34 @@ function parse() {
       firstMeetLi = true;
     }
   }
-
   let newHtmlStr = htmlArr.join('\n');
   return newHtmlStr;
 }
 
-
 function dealChildren() {
-  // 处理嵌套的li(bug之后改)
-  let ulArr = contentWrapper.getElementsByTagName("ul");
-  for (let i = 0; i < ulArr.length; i++) {
-    let liArr = ulArr[i].getElementsByTagName("li");
-    let lcArr = ulArr[i].getElementsByClassName("children");
-    for (let j = 0; j < ulArr.length; j++) {
-      if (liArr[j] && liArr[j].className === "") {
-        for (let k = 0; k < lcArr.length; k++) {
-          liArr[j].appendChild(lcArr[k]);
+  // 处理嵌套的li
+  const fatherUl = contentWrapper.getElementsByTagName("ul");
+  const fatherOl = contentWrapper.getElementsByTagName("ol");
+  for (let i = 0; i < fatherUl.length; i++) {
+    let liArr = fatherUl[i].getElementsByTagName("li");
+    let childrenUl = fatherUl[i].getElementsByClassName("childrenUl");
+    let k = 0;
+    for (let j = 0; j < liArr.length; j++) {
+      if (liArr[j].className === "father") {
+        if (childrenUl[k]) {
+          liArr[j].appendChild(childrenUl[k++]);
+        }
+      }
+    }
+  }
+  for (let i = 0; i < fatherOl.length; i++) {
+    let liArr = fatherOl[i].getElementsByTagName("li");
+    let childrenOl = fatherOl[i].getElementsByClassName("childrenOl");
+    let k = 0;
+    for (let j = 0; j < liArr.length; j++) {
+      if (liArr[j].className === "father") {
+        if (childrenOl[k]) {
+          liArr[j].appendChild(childrenOl[k++]);
         }
       }
     }
@@ -127,7 +278,7 @@ function dealChildren() {
 
 // 更替preview中的内容
 function setValue() {
-  let htmlStr = parse();
+  let htmlStr = parseEl();
   contentWrapper.innerHTML = htmlStr;
   dealChildren();
 }
@@ -158,3 +309,20 @@ function prevTab(event) {
     this.setSelectionRange(newStart, newEnd);
   }
 }
+
+// 存储title
+function saveTitle() {
+  localStorage.setItem('title', ip.value);
+}
+
+// 下载，明天理解此代码
+function download(content, filename) {
+  var eleLink = document.createElement('a');
+  eleLink.download = filename;
+  eleLink.style.display = 'none';
+  var blob = new Blob([content]);
+  eleLink.href = URL.createObjectURL(blob);
+  document.body.appendChild(eleLink);
+  eleLink.click();
+  document.body.removeChild(eleLink);
+};
